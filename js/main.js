308e8736ae47180d6d7611259dd75a677cdb62b1
Capitan-Mugetsu <raxmonberdiyevlola@gmail.com>
2022-03-26 00:40:13 +0500

    add new details

--- js/main.js
+++ js/main.js
@@ -18,3 +18,24 @@ function testimonialsSlider() {
 }
 
 testimonialsSlider()
+
+/*-------------------------
+    course preview video
+--------------------------- */
+
+function coursePreviewVideo() {
+    const coursePreviewModal = document.querySelector('.js-course-preview-modal')
+
+    if(coursePreviewModal) {
+        coursePreviewModal.addEventListener('shown.bs.modal', function() {
+            this.querySelector('.js-course-preview-video').play()
+            this.querySelector('.js-course-preview-video').currentTime = 0
+        })
+
+        coursePreviewModal.addEventListener('hide.bs.modal', function() {
+            this.querySelector('.js-course-preview-video').pause()
+        })
+    }
+}
+
+coursePreviewVideo()
